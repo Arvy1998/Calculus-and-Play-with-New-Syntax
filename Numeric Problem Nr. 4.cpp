563028c4451a56b2dc8b0563d30d20f5269ba831
Arweedas <35611633+Arvy1998@users.noreply.github.com>
2018-05-20 13:07:49 +0300

    Update Numeric Problem Nr. 4.cpp

--- Numeric Problem Nr. 4.cpp
+++ Numeric Problem Nr. 4.cpp
@@ -1,3 +1,23 @@
+/*
+You are given an N x M size grid where N is the number of rows and M is the number of columns. 
+Your task is to calculate in how many ways you can go from the upper-left corner to the bottom-right corner 
+if you can only move down and to the right.
+
+Input:
+
+The input consists of two space-separated integers N and M (1 ≤ N, M ≤ 30).
+
+Output:
+
+Print out one integer - the answer to the given problem.
+
+Examples:
+Input: 1 1
+Output: 2
+Input: 4 4
+Output: 70
+*/
+
 #include <iostream>
 #include <cmath>
 
@@ -24,7 +44,7 @@ int main()
 long long n, m, x, c;
 	cin >> n >> m;
 	c = n + m;
-	cout << f(c) / f(n) * f(m);
+	cout << f(c) / f(n) * f(m); // (M+N)!/(M!N!)
 	return 0;
 }
 
