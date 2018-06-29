6fa81958bd8349c92a2f00646840a9d17bc85b03
Arvydas <35611633+Arvy1998@users.noreply.github.com>
2018-06-29 22:05:52 +0300

    Update Lowercase & Uppercase Problem.cpp

--- Lowercase & Uppercase Problem.cpp
+++ Lowercase & Uppercase Problem.cpp
@@ -1,3 +1,22 @@
+/*
+Gooby hates inconsistencies, therefore words like
+"GoOBy" or "HeLLo" bother him a lot, since the 
+letters differ in case (some are lowercase, 
+some are uppercase). Therefore, he wants to 
+invent a browser extension to make all words
+pretty. A word is pretty in Gooby's eyes, if 
+all of its letters are of the same case. For 
+example, "gooby" and "GOOBY" are both pretty 
+words. Gooby wants to save resources and perform
+as few case-changing operations as possible.
+Therefore, the word "GOOby" should become 
+"GOOBY", and the word "Gooby" should become 
+"gooby". If turning the word to uppercase and
+lowercase takes the same amount of operations,
+the extension should use lowercase. Please help
+Gooby to write the main function of this extension!
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <vector>
